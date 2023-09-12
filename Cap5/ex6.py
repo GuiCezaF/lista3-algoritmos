979a1e1646955e2877ab239ffa18daa3f6c27b65
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-09-12 22:24:13 +0000

    exercicio 6 resolvido

--- Cap5/ex6.py
+++ Cap5/ex6.py
@@ -6,3 +6,48 @@ calculados com números diferentes de pontos.
 Sua tabela deve possuir ao menos 10 valores diferentes.
 Use range() para gerar os diversos valores de entrada da função.
 '''
+import random as r
+import math as m
+
+def gera_coordenadas_aleatorias():
+    '''Gera par de coordenadas aleatórias.'''
+
+    x = r.random()
+
+    y = r.random()
+
+    return x, y
+
+def coordenadas_dentro_circulo(x, y):
+    '''Testa se coordenadas estão dentro do círculo de raio 1.'''
+
+    return m.hypot(x, y) < 1
+
+def calcula_pi(n):
+    '''Calcula pi e o erro associado a partir de n pontos.'''
+    conta_circulo = 0
+
+    for _ in range(n):
+        x, y = gera_coordenadas_aleatorias()
+
+        if coordenadas_dentro_circulo(x, y):
+            conta_circulo += 1
+
+    pi = 4 * conta_circulo / n
+    erro = m.fabs(pi - m.pi)
+
+    return pi, erro
+
+def main():
+    num_valores_pi = 10
+    tabela_pi = []
+    
+    for num_pontos in range(1, num_valores_pi + 1):
+        pi, erro = calcula_pi(num_pontos)
+        tabela_pi.append((num_pontos, pi, erro))
+
+    print("Pontos | Pi | Erro")
+    for linha in tabela_pi:
+        print(f"{linha[0]:.2f} | {linha[1]:.2f} | {linha[2]:.4f}")
+
+main()
