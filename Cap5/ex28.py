0033ebf731a25b0b8ca9dd09d91391a16e49f7a2
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-09-14 23:11:18 +0000

    correção ex28

--- Cap5/ex28.py
+++ Cap5/ex28.py
@@ -2,15 +2,6 @@
 Escreva um programa que crie uma matriz tridimensional
 usando o mecanismo de lista por compreensão.
 '''
-dim_x = 3
-dim_y = 3
-dim_z = 3
+trid = [[[j for j in range(i)] for i in range(3)]]
 
-matriz_3d = [[[0 for _ in range(dim_z)] for _ in range(dim_y)] for _ in range(dim_x)]
-
-for x in range(dim_x):
-    for y in range(dim_y):
-        for z in range(dim_z):
-            print(f"matriz_3d[{x}][{y}][{z}] = {matriz_3d[x][y][z]}")
-
-
+print(trid)
